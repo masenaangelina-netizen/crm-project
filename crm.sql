f778e25204dc5d5ec8b422dbc30fd2662e4b88f7
masenaangelina-netizen <masena.angelina@decathlon.com>
2026-06-28 21:57:37 +0800

    Update date range in CRM SQL query

--- crm.sql
+++ crm.sql
@@ -11,7 +11,7 @@ JOIN
     `twdatawarehouse-bsv8.dashboard_data_new.tw_member_new` AS member
     ON sales.member_id = member.member_id
 WHERE 
-    sales.gmv_recorded_date BETWEEN DATE('2025-04-01') AND DATE('2026-06-30')
+    sales.gmv_recorded_date BETWEEN DATE('2026-04-01') AND DATE('2026-06-30')
     AND sales.member_active_category = 'awaken'
 GROUP BY 
     1, 2, 3
